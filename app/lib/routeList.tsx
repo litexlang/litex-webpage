f2ece44d6556c7f31e616fe7ff4e8235c65885ac
kidhero <scanfandprintf@icloud.com>
2025-01-12 13:50:41 +0800

    - add github link in layout

--- app/lib/routeList.tsx
+++ app/lib/routeList.tsx
@@ -1,5 +1,8 @@
-import { Home, Terminal } from "@mui/icons-material";
+import { GitHub, Home, Terminal } from "@mui/icons-material";
 
-const routeList = [{ title: "Homepage", path: "/", icon: <Home /> },{ title: "Playground", path: "/playground", icon: <Terminal /> }];
+const routeList = [
+    { title: "Homepage", path: "/", icon: <Home /> },
+    { title: "Playground", path: "/playground", icon: <Terminal /> },
+    { title: "Github", path: "https://github.com/litexlang/tslitex", icon: <GitHub /> }];
 
 export default routeList;
