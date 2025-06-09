911a5de8eb1968f0e4a050b334f5309621d66d61
kidhero <scanfandprintf@icloud.com>
2025-06-09 23:48:15 +0800

    fix: update Playground to use initial code from litexExamples instead of Redux state

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,18 +1,15 @@
 "use client";
 
 import { Container, Toolbar } from "@mui/material";
-import { useAppSelector } from "../lib/browser/store/hooks";
 import Playground from "../ui/playground";
+import litexExamples from "../lib/browser/litexExamples";
 
 export default function Home() {
-
-  // redux vars
-  const code = useAppSelector((state) => state.code.value);
 
   return (
     <Container component={"main"} sx={{ p: 2 }} maxWidth={false}>
       <Toolbar />
-      <Playground height={'calc(100vh - 160px)'} initCode={code} />
+      <Playground height={'calc(100vh - 160px)'} initCode={litexExamples[0].code} />
     </Container>
   );
 }
