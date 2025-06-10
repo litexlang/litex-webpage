60c014bfdb21d812f9ff420099bb9a6ce833bfe8
kidhero <scanfandprintf@icloud.com>
2025-06-10 17:42:59 +0800

    fix: update litexExecutor to write temporary files for execution

--- app/lib/server/litexExecutor.tsx
+++ app/lib/server/litexExecutor.tsx
@@ -1,8 +1,11 @@
 import { exec } from 'child_process';
+import { writeFileSync } from 'fs';
 
 export function litexExecutor(litexString: string): Promise<string> {
+    const tmpFileDir = process.env.LITEX_TMP_DIR + "/" + new Date().toISOString()
+    writeFileSync(tmpFileDir, litexString, { encoding: 'utf-8' });
     return new Promise((resolve, reject) => {
-        exec(process.env.LITEX_DIR + ` -e \"` + litexString + `\"`, (error, stdout, stderr) => {
+        exec(process.env.LITEX_DIR + ` -f ` + tmpFileDir, (error, stdout, stderr) => {
             if (error) {
                 reject(`Error executing command: ${error.message}`);
             } else if (stderr) {
