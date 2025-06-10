9549882d8420924d8f36d2450aa2086f41c741df
kidhero <scanfandprintf@icloud.com>
2025-06-10 17:49:54 +0800

    fix: correct temporary file path concatenation in litexExecutor

--- app/lib/server/litexExecutor.tsx
+++ app/lib/server/litexExecutor.tsx
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 import { writeFileSync } from 'fs';
 
 export function litexExecutor(litexString: string): Promise<string> {
-    const tmpFileDir = process.env.LITEX_TMP_DIR + "/" + new Date().toISOString()
+    const tmpFileDir = process.env.LITEX_TMP_DIR + new Date().toISOString()
     writeFileSync(tmpFileDir, litexString, { encoding: 'utf-8' });
     return new Promise((resolve, reject) => {
         exec(process.env.LITEX_DIR + ` -f ` + tmpFileDir, (error, stdout, stderr) => {
