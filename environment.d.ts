f84afd534161c9c99f2b373f7e652925f9ff243f
kidhero <scanfandprintf@icloud.com>
2025-07-09 17:30:32 +0800

    feat: update environment variables in ProcessEnv interface and rename AppLayout file

--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,13 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        DOC_DIR: string;
-      }
+  namespace NodeJS {
+    interface ProcessEnv {
+      DOC_DIR: string;
+      DEMO_DIR: string;
+      LITEX_DIR: string;
+      LITEX_TMP_DIR: string;
+      LITEX_VERSION: string;
     }
   }
-  
-  export {};
-  
+}
+
+export {};
