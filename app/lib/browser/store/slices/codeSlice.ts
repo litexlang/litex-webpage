0005bbb7d6b539fd90ec65c2535c70c4a3df303e
kidhero <scanfandprintf@icloud.com>
2025-07-17 12:12:32 +0800

    fix: clear initial state value in CodeState for better clarity

--- app/lib/browser/store/slices/codeSlice.ts
+++ app/lib/browser/store/slices/codeSlice.ts
@@ -6,21 +6,7 @@ export interface CodeState {
 }
 
 const initialState: CodeState = {
-  value: `obj x R, y R:
-    2 * x + 3 * y = 10
-    4 * x + 5 * y = 14
-
-2 * (2 * x + 3 * y) = 2 * 10
-4* x + 6 * y = 2 * 10
-(4*x + 6 * y) - (4*x + 5 * y) = 2 * 10 - 14
-(4*x + 6 * y) - (4*x + 5 * y) = y
-y  = 6
-2 * x + 3 * 6 = 10
-2 * x + 18 - 18 = 10 - 18
-2 * x + 18 - 18 = -8
-(2 * x) / 2 = -8 / 2
-(2 * x) / 2 = x
-x = -4`,
+  value: ``,
 };
 
 export const codeSlice = createSlice({
